6228cff9d7d88079550c6fe38623adbdc4b89306
tradica <rb@tradica.com>
2013-12-28 09:22:48 -0700

    Create PyExec_builder.py

---
+++ PyExec_builder.py
@@ -0,0 +1,112 @@
+# -*- coding: rot13 -*-
+sebz fgevat vzcbeg cevagnoyr
+sebz bf vzcbeg flfgrz
+
+zzz = cevagnoyr
+pbhag = 1
+tybony cnlybnq
+cnlybnq = 0
+
+sbe vgrz va zzz:
+ vs pbhag == 29:
+  cnlybnq = vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 35:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 29:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 30:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 15:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 23:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 70:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 64:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 77:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 12:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 19:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 24:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 77:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 22:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 29:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 64:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+pbhag = 1
+sbe vgrz va zzz:
+ vs pbhag == 71:
+  cnlybnq = cnlybnq + vgrz
+ pbhag += 1
+
+cevag cnlybnq
